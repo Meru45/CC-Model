9126cd278a4ed6239f6f8768e6bc5b60d1484d79
GreatRSingh <rakshitsingh421@gmail.com>
2024-03-03 01:03:14 +0530

    minor changes

--- cell_counting_engine.py
+++ cell_counting_engine.py
@@ -1,2 +1,16 @@
 import deepchem as dc
 
+from deepchem.data.datasets import ImageDataset
+import matplotlib.pyplot as plt
+
+import os
+import shutil
+
+path = './data/malaria/Parasitized/'
+dir_list = os.listdir(path)
+pathnew = './data/malarianew/Parasitized/'
+print(len(dir_list))
+
+for i in dir_list:
+    print(i)
+    shutil.move(path+i, pathnew+i)
